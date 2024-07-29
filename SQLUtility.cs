5a66da6a3e81fc549ddaf65a38fa777ce9a62259
yaakoveidelman <yeidelman@eidelmans.com>
2024-07-29 10:55:49 -0400

    HW 18

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -7,23 +7,38 @@ namespace CPUFramework
     public class SQLUtility
     {
         public static string ConnectionString = "";
+
+        public static SqlCommand GetSqlCommand(string sprocname)
+        {
+            SqlCommand cmd;
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                cmd = new SqlCommand(sprocname, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                conn.Open();
+                SqlCommandBuilder.DeriveParameters(cmd);
+            }
+            return cmd;
+        }
+
+        public static DataTable GetDataTable(SqlCommand cmd)
+        {
+            Debug.Print("-----" + Environment.NewLine + cmd.CommandText);
+            DataTable dt = new();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                cmd.Connection = conn;
+                SqlDataReader dr = cmd.ExecuteReader();
+                dt.Load(dr);
+            }
+            SetAllColumnsAllowNull(dt);
+            return dt;
+        }
+
         public static DataTable GetDataTable(string sqlstatement)
         {
-            Debug.Print(sqlstatement);
-            DataTable dt = new();
-            SqlConnection conn = new();
-            conn.ConnectionString = ConnectionString;
-            conn.Open();
-
-            SqlCommand cmd = new();
-            cmd.Connection = conn;
-            cmd.CommandText = sqlstatement;
-            SqlDataReader dr = cmd.ExecuteReader();
-            dt.Load(dr);
-
-            SetAllColumnsAllowNull(dt);
-
-            return dt;
+            return GetDataTable(new SqlCommand(sqlstatement));
         }
 
         public static void ExecuteSQL(string sql)
@@ -36,9 +51,9 @@ namespace CPUFramework
             int n = 0;
 
             DataTable dt = GetDataTable(sql);
-            if(dt.Rows.Count > 0 && dt.Columns.Count > 0)
+            if (dt.Rows.Count > 0 && dt.Columns.Count > 0)
             {
-                if(dt.Rows[0][0] != DBNull.Value)
+                if (dt.Rows[0][0] != DBNull.Value)
                 {
                     object o = int.TryParse(dt.Rows[0][0].ToString(), out n);
                 }
@@ -49,7 +64,7 @@ namespace CPUFramework
 
         private static void SetAllColumnsAllowNull(DataTable dt)
         {
-            foreach(DataColumn c in dt.Columns)
+            foreach (DataColumn c in dt.Columns)
             {
                 c.AllowDBNull = true;
             }
