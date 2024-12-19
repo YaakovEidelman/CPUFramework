81483ee500202d5fef216ce5b6d1c4365d459a7e
yaakoveidelman <yeidelman@eidelmans.com>
2024-12-19 11:16:08 -0500

    added properties to bizRecipe and updated some tests

--- bizObject.cs
+++ bizObject.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
+﻿using System.Data.SqlClient;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Reflection;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Security.Cryptography;
 
 namespace CPUFramework
 {
